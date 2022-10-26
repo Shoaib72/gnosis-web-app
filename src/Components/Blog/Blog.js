8096ae50ae818992311568e573378b354a242f11
Shoaib72 <shoaib.953872@gmail.com>
2022-10-26 10:43:45 +0100

    routes-added

--- src/Components/Blog/Blog.js
+++ src/Components/Blog/Blog.js
@@ -3,7 +3,7 @@ import React from 'react';
 const Blog = () => {
     return (
         <div>
-
+            <h2>This is Blog</h2>
         </div>
     );
 };
--- src/Routes/Routes/Routes.js
+++ src/Routes/Routes/Routes.js
@@ -1,5 +1,13 @@
 import { createBrowserRouter } from "react-router-dom";
+import Blog from "../../Components/Blog/Blog";
+import CheckOut from "../../Components/CheckOut/CheckOut";
+import CourseDetails from "../../Components/CourseDetails/CourseDetails";
+import Courses from "../../Components/Courses/Courses";
+import FAQ from "../../Components/FAQ/FAQ";
+import Login from "../../Components/Login/Login";
+import Register from "../../Components/Register/Register";
 import Main from "../../Layout/Main";
+import Home from './../../Components/Home/Home';
 
 export const routes = createBrowserRouter([
     {
@@ -7,7 +15,40 @@ export const routes = createBrowserRouter([
         element: <Main></Main>,
         children: [
             {
-
+                path: '/',
+                element: <Home></Home>,
+                loader: () => fetch('https://gnosis-web-app-server.vercel.app/courses')
+            },
+            {
+                path: '/courses',
+                element: <Courses></Courses>,
+                loader: () => fetch('https://gnosis-web-app-server.vercel.app/courses')
+            },
+            {
+                path: '/course/:id',
+                element: <CourseDetails></CourseDetails>,
+                loader: ({ params }) => fetch(`https://gnosis-web-app-server.vercel.app/courses/${params.id}`)
+            },
+            {
+                path: '/checkout/:id',
+                element: <CheckOut></CheckOut>,
+                loader: ({ params }) => fetch(`https://gnosis-web-app-server.vercel.app/checkout/${params.id}`)
+            },
+            {
+                path: '/blog',
+                element: <Blog></Blog>
+            },
+            {
+                path: '/faq',
+                element: <FAQ></FAQ>
+            },
+            {
+                path: '/login',
+                element: <Login></Login>
+            },
+            {
+                path: '/register',
+                element: <Register></Register>
             }
         ]
     }
