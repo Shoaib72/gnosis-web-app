ad95e61a89c94608b9616c5907efbeff8fe929d1
Shoaib72 <shoaib.953872@gmail.com>
2022-10-27 12:27:47 +0100

    onAuthStateChangeAdded

--- src/Contexts/AuthProvider/AuthProvider.js
+++ src/Contexts/AuthProvider/AuthProvider.js
@@ -1,15 +1,25 @@
 import React from 'react';
-import { getAuth } from 'firebase/auth';
+import { getAuth, onAuthStateChanged } from 'firebase/auth';
 import { createContext } from 'react';
 import app from '../../Components/Firebase/firebase.config';
 import { useState } from 'react';
+import { useEffect } from 'react';
 export const AuthContext = createContext();
 const auth = getAuth(app);
 
 const AuthProvider = ({ children }) => {
     const [user, setUser] = useState(null);
+    useEffect(() => {
+        const unsubscribe = onAuthStateChanged(auth, (currentUser) => {
+            console.log('User inside state change', currentUser);
+            setUser(currentUser)
+        });
+        return () => {
+            unsubscribe()
+        }
+    }, [])
 
-    console.log(user)
+
     const authInfo = { user, setUser }
     return (
         <AuthContext.Provider value={authInfo}>
